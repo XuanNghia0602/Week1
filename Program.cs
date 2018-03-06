67893e2d05478e9bd01a7db5b495dd5ff0984287
XuanNghia0602 <xuannghia0702@gmail.com>
2018-03-06 17:42:29 +0700

    First commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace Bai_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String strHostName = Dns.GetHostName();
+
+            Console.WriteLine("Host Name: " + strHostName);
+          //  IPHostEntry iphostentry = Dns.GetHostByName(strHostName);
+            int nIP = 0;
+            IPHostEntry iphostentry = Dns.Resolve(strHostName);
+            foreach (IPAddress ipaddress in iphostentry.AddressList)
+            {
+                Console.WriteLine("IP #" + ++nIP + ": " +
+                                  ipaddress.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
